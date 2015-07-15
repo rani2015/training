8007e74dcd8407ab75c537f5fc5fd17e20d7c0b4
vijayalaxmiyanala <vijayalaxmiyanala@hotmail.com>
2015-07-14 21:08:01 -0400

    vijaya commit

--- src/spring_di/App.java
+++ src/spring_di/App.java
@@ -14,5 +14,13 @@ public class App {
 		System.out.println("----end--");
 		context.close();
 	}
-
+public static void main2(String[] args) {
+		
+		AbstractApplicationContext context =new ClassPathXmlApplicationContext("bean.xml");
+		System.out.println("----start--");
+		TextEditor te  =(TextEditor)context.getBean("textEditor");
+		te.spellCheck();
+		System.out.println("----end--");
+		context.close();
+	}
 }
